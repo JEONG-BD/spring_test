62f881efdada6319f931d2315ff249225a5bd614
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-25 19:10:57 +0900

    health controller test 코드 작성

---
+++ demo/src/test/java/com/example/demo/controller/HealthCheckTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureTestDatabase
+public class HealthCheckTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @Order(1)
+    public void 헬스_체크_응답이_200으로_내려온다() throws Exception {
+        mockMvc.perform(get("/health_check.html"))
+                .andExpect(status().isOk());
+
+    }
+}
